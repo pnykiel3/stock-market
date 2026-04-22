391bf79da4907b1440c1c6e15e25e500d9bfbe0a
pawelnykiel <p.nykiel@icloud.com>
2026-04-22 13:08:07 +0200

    changed String to enum TransactionType

--- src/main/java/com/example/stock_market/dto/AuditLogDto.java
+++ src/main/java/com/example/stock_market/dto/AuditLogDto.java
@@ -1,8 +1,9 @@
 package com.example.stock_market.dto;
 
+import com.example.stock_market.model.TransacionType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record AuditLogDto(String type,
+public record AuditLogDto(TransacionType type,
                           @JsonProperty("wallet_id") String walletId,
                           @JsonProperty("stock_name") String stockName) {
 }
