ee8fac60e67eee2849cbd85a293e367a5dc60d5d
pawelnykiel <p.nykiel@icloud.com>
2026-04-29 22:36:43 +0200

    JsonProperty for enum TransactionType

--- src/main/java/com/example/stock_market/model/TransactionType.java
+++ src/main/java/com/example/stock_market/model/TransactionType.java
@@ -1,6 +1,8 @@
 package com.example.stock_market.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum TransactionType {
-    BUY,
-    SELL
+    @JsonProperty("buy") BUY,
+    @JsonProperty("sell") SELL
 }
