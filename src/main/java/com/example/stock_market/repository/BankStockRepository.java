7ffce86c5df856311fd4a3ca1098abb285fc2360
pawelnykiel <p.nykiel@icloud.com>
2026-04-29 21:15:07 +0200

    BankStockRepository new function for service

--- src/main/java/com/example/stock_market/repository/BankStockRepository.java
+++ src/main/java/com/example/stock_market/repository/BankStockRepository.java
@@ -1,7 +1,17 @@
 package com.example.stock_market.repository;
 
 import com.example.stock_market.model.BankStock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BankStockRepository extends JpaRepository<BankStock, String> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT b FROM BankStock b WHERE b.name = :name")
+    Optional<BankStock> findByIdForUpdate(@Param("name") String name);
 }
