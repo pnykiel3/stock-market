84f58f81b6f2a8cc0afc11cf14c5cb7a394e01b5
pawelnykiel <p.nykiel@icloud.com>
2026-04-30 12:01:18 +0200

    Stock Market test

--- src/test/java/com/example/stock_market/StockMarketApplicationTests.java
+++ src/test/java/com/example/stock_market/StockMarketApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.stock_market;
 
+import com.example.stock_market.integration.TestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfig.class)
 class StockMarketApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
